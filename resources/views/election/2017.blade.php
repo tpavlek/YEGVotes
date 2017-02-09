907abf359d87624292350a2a76b884e4f7c9d6a7
Troy Pavlek <troy@tpavlek.me>
2017-02-09 16:31:30 -0700

    Secure d3

--- resources/views/election/2017.blade.php
+++ resources/views/election/2017.blade.php
@@ -67,7 +67,7 @@
             }
 
         </style>
-        <script src="http://d3js.org/d3.v3.min.js"></script>
+        <script src="https://d3js.org/d3.v3.min.js"></script>
         <script>
 
             //Map dimensions (in pixels)
